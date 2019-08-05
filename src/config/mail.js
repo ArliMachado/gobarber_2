16a8d79b2ef2d225c0a4288a29f0d38fe372f6f0
Arli <arli.machado@gmail.com>
2019-08-05 16:45:49 -0300

    Enviando email após o cancelamento do agendamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'c800cffd713c73',
+    pass: '962236c28c7b7e',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
